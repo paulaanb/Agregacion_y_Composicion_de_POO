1ac788d832d4772bc31f95723a537cdbe207621c
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-23 22:22:16 +0100

    Definimos las clases pared, ventana, casa y cortina

--- Clases/Ejercicio3.py
+++ Clases/Ejercicio3.py
@@ -3,3 +3,31 @@
 #Enunciado: modifique las clases Ventana y ParedCortina para que usen esta nueva clase-interfaz Cristal.
 #Enunciado: modifique el código para que el programa funcione de nuevo.
 
+
+#Definimos las clases que vamos a utilizar, Pared, Ventana, Casa y Cortina
+class Pared:
+    def __init__(self, orientacion):
+        self.orientacion = orientacion
+        
+class Ventana:
+    def __init__(self, pared, superficie, acristalamiento):
+        self.pared = pared
+        self.superficie = superficie
+        self.pared.ventanas.append(self)
+        if acristalamiento is None:
+            raise Exception("El acristamlamiento de la ventana es obligatoria")
+        self.acristalamiento = acristalamiento
+
+class Casa:
+    def __init__(self, paredes, orientacion, superficie):
+        super().__init__(orientacion, superficie)
+        self.paredes = paredes
+    
+    def superficie_acristalada(self):
+        return sum(self.paredes.superficie)
+
+class Cortina:
+    def __init__(self, orientacion, superficie):
+        Pared.__init__(self, orientacion)
+        Ventana.__init__(self, self, superficie, "Ninguna")
+
