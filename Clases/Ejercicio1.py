749870f60c2a6630252bed8590fd317a179aeabe
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-23 21:57:59 +0100

    Codigo 1 terminado

--- Clases/Ejercicio1.py
+++ Clases/Ejercicio1.py
@@ -1,3 +1,20 @@
 #Enunciado: modelar lo siguiente. Una empresa es propietaria de varios edificios y emplea a varios empleados. Un edificio está necesariamente ubicado en una ciudad y una ciudad está formada por varios edificios. Empresa, empleado, ciudad y edificio tienen cada uno un nombre. Estas ciudades incluyen New York, donde se encuentran los edificios A y B, y Los Ángeles, donde está el edificio C. Estos tres edificios son propiedad de YooHoo! que emplea a los Sres. Martin, Salim y la Sra. Xing.
 #Una vez definidas estas entidades, imagine que su programa es una película estadounidense de catástrofes, en la que se destruye New York. Implemente este evento para que todas las entidades del juego tengan en cuenta las consecuencias de este cataclismo.
 
+#Empezamos a crear el codigo definiendo la clase edificio y la clase empresa
+class Edificio():
+  def __init__(self, edificio):
+    self.edificio = edificio
+ class Empresa():
+  def ciudad():
+      self.ciudad = ciudad
+      #Establecemos las relacioness
+      print("¿Que ciudad desea destruir, New_York o Los_Angeles?")
+      if ciudad == New_York
+      print("New_York se ha destruido junto con los edificios A y B, y el Mr.Salim y la Sra.Xing")
+      if ciudad == Los_Angeles
+      print("Los_Angeles se ha destruido junto con el edificio C y el Sr.Martin")
+   
+#Comando para ejecutar el programa
+print("Ejercicio1")
+print(destruccion.ciudad("New_York"))
